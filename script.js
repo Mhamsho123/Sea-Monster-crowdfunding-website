5c0b2ab2aa63b29f0fa08c0f6dabb3ca51d763b2
Mohamad Hamsho <hamsho1030@gmail.com>
2026-05-24 14:57:55 -0400

    added game data javascript file

---
+++ script.js
@@ -0,0 +1,17 @@
+const games = [
+    {
+        name: "Ocean Explorer",
+        raised: 5000,
+        goal: 10000
+    },
+    {
+        name: "Deep Sea Battle",
+        raised: 15000,
+        goal: 12000
+    },
+    {
+        name: "Monster Escape",
+        raised: 7000,
+        goal: 7000
+    }
+]
